27cccf6d63d02489958b74d3774220fc6fb7f3c1
syafiraJafar <56532146+syafiraJafar@users.noreply.github.com>
2020-12-20 23:37:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_20180140070_SyafiraJafar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
